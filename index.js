8dea4f1236ada280ea38ea12f9a0eec94d2689d8
Danny <Danny007111@hotmail.com>
2022-03-18 22:05:56 -0500

    Saving work

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
 // Code your solutions in this file
+
+
+function writeCards(arr1, name){
+
+    const emptyA = [];
+
+    for (let i = 0; i < arr1.length; i++){
+
+        emptyA.push(`Thank you, ${arr1[i]}, for the wonderful ${name} gift!`)
+        
+    };
+    return emptyA;
+};
+
+writeCards(["Ada", "Brendan", "Ali"], "birthday");
