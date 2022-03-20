446dd821496805a2b8685a017ca2ba790ccd74a6
Danny Rodriguez <danny007111@hotmail.com>
2022-03-20 04:47:28 -0500

    Completed assignment

--- index.js
+++ index.js
@@ -1,16 +1,19 @@
 // Code your solutions in this file
 
-
 function writeCards(arr1, name){
-
     const emptyA = [];
-
     for (let i = 0; i < arr1.length; i++){
-
         emptyA.push(`Thank you, ${arr1[i]}, for the wonderful ${name} gift!`)
-        
     };
     return emptyA;
 };
 
-writeCards(["Ada", "Brendan", "Ali"], "birthday");
+function countDown(numb){
+    let i = 0;
+    while (i <= numb) {
+        console.log(numb);
+        numb--;
+        };
+    };
+
+
